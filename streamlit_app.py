53f0ce45a74c56e40796637950f11be0e72764e9
Atharva Wagh <81549203+AtharvaW251102@users.noreply.github.com>
2023-10-17 13:44:42 +0530

    Update streamlit_app.py
    Added List

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,4 @@
 import streamlit
-import pandas
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.dataframe(my_fruit_list)
-
 
 streamlit.title('My Parents New Healthy Diner')
 
@@ -13,3 +9,6 @@ streamlit.text(' 🐔 Hard-Boiled free-range Egg')
 streamlit.text(' 🥑🍞Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
